fe6d9762589d8bbf216e176f144f6ef2df424403
Andrius Degutis <andriusdgt@gmail.com>
2020-04-20 20:30:17 +0300

    init a working hello world app

---
+++ src/main/java/com/andriusdgt/thedotsbackend/TheDotsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.andriusdgt.thedotsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDotsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDotsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andriusdgt/thedotsbackend/TheDotsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriusdgt.thedotsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheDotsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
