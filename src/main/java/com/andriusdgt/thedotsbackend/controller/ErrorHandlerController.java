e1509038c71be9555de4513388c79d4c92e09cd7
Andrius Degutis <andriusdgt@gmail.com>
2020-04-23 00:09:28 +0300

    error stacktraces are hidden from the world

---
+++ src/main/java/com/andriusdgt/thedotsbackend/controller/ErrorHandlerController.java
@@ -0,0 +1,25 @@
+package com.andriusdgt.thedotsbackend.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ErrorHandlerController implements ErrorController {
+
+    private static final String ERROR_PATH = "/error";
+
+    @RequestMapping(ERROR_PATH)
+    Map<String, String> error() {
+        return new HashMap<>(Map.of("Message", "Task failed successfully"));
+    }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+}
