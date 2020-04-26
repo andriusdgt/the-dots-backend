dd0cf79c9fd48ac015796145c050ba2dd0dfcd16
Andrius Degutis <andriusdgt@gmail.com>
2020-04-26 23:25:16 +0300

    exposed server to rest queries across origins

---
+++ src/main/java/com/andriusdgt/thedotsbackend/config/WebConfigurer.java
@@ -0,0 +1,19 @@
+package com.andriusdgt.thedotsbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+class WebConfigurer implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+            .exposedHeaders("Content-Disposition");
+    }
+
+}
